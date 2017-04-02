4db9a2bd5b7292f411893745c5198257daf51237
lizhonghui <lzhspace@gmail.com>
2017-04-02 23:33:47 +0800

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: "airbnb-base",
+  rules: {
+    'comma-dangle': 0,
+    'quotes': 0,
+    'new-cap': 0,
+    'eol-last': 0,
+    'global-require': 0,
+    'no-plusplus': 0,
+    'no-mixed-operators': 0,
+    'no-underscore-dangle': 0,
+    'no-multi-assign': 0,
+    'no-ex-assign': 0,
+    'default-case': 0,
+    'class-methods-use-this': 0,
+    'no-unused-expressions': [2, {
+      allowShortCircuit: true,
+      allowTernary: true
+    }],
+    'no-param-reassign': 0,
+    "import/no-extraneous-dependencies": ["error", {
+      "devDependencies": ['**/tests/**/*.js'],
+    }],
+  }
+};
